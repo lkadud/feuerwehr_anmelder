ebc48e9a3227d57949b56e2a18137a24f7002233
Nick Thamm <nickthamm@gmx.de>
2026-01-21 11:57:59 +0100

    minor changes to build.sh

--- build.sh
+++ build.sh
@@ -2,10 +2,12 @@
 
 #https://andreafortuna.org//2017/12/27/how-to-cross-compile-a-python-script-into-a-windows-executable-on-linux/
 #https://stackoverflow.com/questions/2950971/packaging-a-python-script-on-linux-into-a-windows-executable
+
 uvx pyinstaller --onefile \
                 --nowindow \
                 --clean \
                 --icon=feuerwehrhelm.ico \
+                --collect-all ttkbootstrap\
                 --paths ./.venv/lib64/python3.13/site-packages/ \
                 --name "Feuerwehr-Anmelder" \
                 --hidden-import="PIL._tkinter_finder" \
